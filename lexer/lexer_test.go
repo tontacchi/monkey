8aecbde3de32b98d7198309974178ffa2ac423c4
tontacchi <andy.tonthat14@gmail.com>
2025-07-02 12:42:41 -0700

    added lexer_test for subset of monkey language. needs tests filled in

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,7 +6,7 @@ import (
 	"monkey/token"
 )
 
-func TestNextToken(t *testing.T) {
+func TestNextTokenBasicSymbols(t *testing.T) {
 	input := "=+(){}[],;"
 
 	tests := []struct {
@@ -24,6 +24,44 @@ func TestNextToken(t *testing.T) {
 		{token.COMMA,  ","},
 		{token.SEMICOLON, ";"},
 		{token.EOF,    ""},
+	}
+
+	lex := New(input)
+
+	for index, test := range tests {
+		testToken := lex.NextToken()
+
+		if test.expectedType != testToken.Type {
+			t.Fatalf("tests[%d] - incorrect token type. expected=%v, got=%v",
+				index, test.expectedType, testToken.Type,
+			)
+		}
+
+		if testToken.Literal != test.expectedLiteral {
+			t.Fatalf("test[%d] - incorrect literal. expected=%v, got=%v",
+				index, test.expectedLiteral, testToken.Literal,
+			)
+		}
+	}
+}
+
+func TestNextTokenSubsetInput(t *testing.T) {
+	input := `let five = 5;
+let ten = 10;
+
+let add = fn(x, y) {
+	x + y;
+};
+
+let result = add(five, ten);
+`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		// fill in from provided repo
+		{token.EOF, ""},
 	}
 
 	lex := New(input)
