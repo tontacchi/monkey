a180a573ddea2dceecb2a4d8e81638e6148dadca
tontacchi <andy.tonthat14@gmail.com>
2025-07-03 10:51:13 -0700

    finalized ch1 tests 1. added Test5 -> full monkey snippet w/ all supported symbols 2. fixed test -> wrapped expected & actual output in ""

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,7 +6,7 @@ import (
 	"monkey/token"
 )
 
-func TestNextTokenBasicSymbols(t *testing.T) {
+func TestNextToken1(t *testing.T) {
 	input := "=+(){}[],;"
 
 	tests := []struct {
@@ -25,27 +25,26 @@ func TestNextTokenBasicSymbols(t *testing.T) {
 		{token.SEMICOLON, ";"},
 		{token.EOF,    ""},
 	}
-
 	lex := New(input)
 
 	for index, test := range tests {
 		testToken := lex.NextToken()
 
 		if test.expectedType != testToken.Type {
-			t.Fatalf("tests[%d] - incorrect token type. expected=%v, got=%v",
+			t.Fatalf("tests[%d] - incorrect token type. expected=\"%v\", got=\"%v\"",
 				index, test.expectedType, testToken.Type,
 			)
 		}
 
 		if testToken.Literal != test.expectedLiteral {
-			t.Fatalf("test[%d] - incorrect literal. expected=%v, got=%v",
+			t.Fatalf("test[%d] - incorrect literal. expected=\"%v\", got=\"%v\"",
 				index, test.expectedLiteral, testToken.Literal,
 			)
 		}
 	}
 }
 
-func TestNextTokenSubsetInput(t *testing.T) {
+func TestNextToken2(t *testing.T) {
 	input := `let five = 5;
 let ten = 10;
 
@@ -104,13 +103,324 @@ let result = add(five, ten);
 		testToken := lex.NextToken()
 
 		if test.expectedType != testToken.Type {
-			t.Fatalf("tests[%d] - incorrect token type. expected=%v, got=%v",
+			t.Fatalf("tests[%d] - incorrect token type. expected=\"%v\", got=\"%v\"",
 				index, test.expectedType, testToken.Type,
 			)
 		}
 
 		if testToken.Literal != test.expectedLiteral {
-			t.Fatalf("test[%d] - incorrect literal. expected=%v, got=%v",
+			t.Fatalf("test[%d] - incorrect literal. expected=\"%v\", got=\"%v\"",
+				index, test.expectedLiteral, testToken.Literal,
+			)
+		}
+	}
+}
+
+func TestNextToken3(t *testing.T) {
+	input := `let five = 5;
+let ten = 10;
+
+let add = fn(x, y) {
+x + y;
+};
+
+let result = add(five, ten);
+!-/*5;
+5 < 10 > 5;
+`
+	tests := []struct{
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
+		{token.ASSIGN, "="},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+	lex := New(input)
+
+	for index, test := range tests {
+		testToken := lex.NextToken()
+
+		if test.expectedType != testToken.Type {
+			t.Fatalf("tests[%d] - incorrect token type. expected=\"%v\", got=\"%v\"",
+				index, test.expectedType, testToken.Type,
+			)
+		}
+
+		if testToken.Literal != test.expectedLiteral {
+			t.Fatalf("test[%d] - incorrect literal. expected=\"%v\", got=\"%v\"",
+				index, test.expectedLiteral, testToken.Literal,
+			)
+		}
+	}
+}
+
+func TestNextToken4(t *testing.T) {
+	input := `let five = 5;
+let ten = 10;
+
+let add = fn(x, y) {
+x + y;
+};
+
+let result = add(five, ten);
+!-/*5;
+5 < 10 > 5;
+
+if (5 < 10) {
+	return true;
+} else {
+	return false;
+}`
+	tests := []struct{
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
+		{token.ASSIGN, "="},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.EOF, ""},
+	}
+	lex := New(input)
+
+	for index, test := range tests {
+		testToken := lex.NextToken()
+
+		if test.expectedType != testToken.Type {
+			t.Fatalf("tests[%d] - incorrect token type. expected=\"%v\", got=\"%v\"",
+				index, test.expectedType, testToken.Type,
+			)
+		}
+
+		if testToken.Literal != test.expectedLiteral {
+			t.Fatalf("test[%d] - incorrect literal. expected=\"%v\", got=\"%v\"",
+				index, test.expectedLiteral, testToken.Literal,
+			)
+		}
+	}
+}
+
+func TestNextToken5(t *testing.T) {
+	input := `let five = 5;
+let ten = 10;
+
+let add = fn(x, y) {
+x + y;
+};
+
+let result = add(five, ten);
+!-/*5;
+5 < 10 > 5;
+
+if (5 < 10) {
+	return true;
+} else {
+	return false;
+}
+
+10 == 10;
+10 != 9;
+`
+	tests := []struct{
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
+		{token.ASSIGN, "="},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.INT, "10"},
+		{token.EQ, "=="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "10"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "9"},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+	lex := New(input)
+
+	for index, test := range tests {
+		testToken := lex.NextToken()
+
+		if test.expectedType != testToken.Type {
+			t.Fatalf("tests[%d] - incorrect token type. expected=\"%v\", got=\"%v\"",
+				index, test.expectedType, testToken.Type,
+			)
+		}
+
+		if testToken.Literal != test.expectedLiteral {
+			t.Fatalf("test[%d] - incorrect literal. expected=\"%v\", got=\"%v\"",
 				index, test.expectedLiteral, testToken.Literal,
 			)
 		}
