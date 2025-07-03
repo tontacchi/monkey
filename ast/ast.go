e67bca69f930bf95ecc8002ca5d0ede740d94312
tontacchi <andy.tonthat14@gmail.com>
2025-07-03 11:50:43 -0700

    implemented Identifier & LetStatement structs. implements Expression & Statement

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,9 @@
 package ast
 
+import (
+	"monkey/token"
+)
+
 //---[ Node Interfaces ]--------------------------------------------------------
 type Node interface {
 	TokenLiteral() string
@@ -30,3 +34,29 @@ func (program *Program) TokenLiteral() string {
 
 	return program.Statements[0].TokenLiteral()
 }
+
+
+type Identifier struct {
+	Token token.Token  // token.IDENT
+	Value string
+}
+
+func (identifier *Identifier) expressionNode() {}  // simplifies using identifiers as RHS values
+
+func (identifier *Identifier) TokenIdentifier() string {
+	return identifier.Token.Literal
+}
+
+
+type LetStatement struct {
+	Token  token.Token  // should always be the token.LET token
+	Name  *Identifier   // variable used in binding
+	Value  Expression   // RHS produces value -> bind to variable
+}
+
+func (let *LetStatement) statementNode() {}
+
+func (let *LetStatement) TokenIdentifier() string {
+	return let.Token.Literal
+}
+
