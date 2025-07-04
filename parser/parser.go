c7c60c71c42a604438e74eeb0ac59283baaf72f0
tontacchi <andy.tonthat14@gmail.com>
2025-07-04 01:11:02 -0700

    implemented more Parser methods 1) ParseProgram() 2) parseStatement() & parseLetStatement() 3) currTokenIs(), peekTokenIs(), expectPeek()

--- parser/parser.go
+++ parser/parser.go
@@ -34,7 +34,19 @@ func New(lex *lexer.Lexer) *Parser {
 //---[ Parser API Methods ]-----------------------------------------------------
 
 func (parser *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for parser.currToken.Type != token.EOF {
+		statement := parser.parseStatement()
+		if statement != nil {
+			program.Statements = append(program.Statements, statement)
+		}
+
+		parser.nextToken()
+	}
+
+	return program
 }
 
 //---[ Parser API Methods ]-----------------------------------------------------
@@ -46,6 +58,58 @@ func (parser *Parser) ParseProgram() *ast.Program {
 func (parser *Parser) nextToken() {
 	parser.currToken = parser.peekToken
 	parser.peekToken = parser.lex.NextToken()
+}
+
+func (parser *Parser) parseStatement() ast.Statement {
+	switch parser.currToken.Type {
+	case token.LET:
+		return parser.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (parser *Parser) parseLetStatement() *ast.LetStatement {
+	statement := &ast.LetStatement{
+		Token: parser.currToken,
+	}
+
+	if !parser.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	statement.Name = &ast.Identifier{
+		Token: parser.currToken,
+		Value: parser.currToken.Literal,
+	}
+
+	if !parser.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO: skipping expressions until semicolon encountered
+	for !parser.currTokenIs(token.SEMICOLON) {
+		parser.nextToken()
+	}
+
+	return statement
+}
+
+func (parser *Parser) currTokenIs(tokenType token.TokenType) bool {
+	return parser.currToken.Type == tokenType
+}
+
+func (parser *Parser) peekTokenIs(tokenType token.TokenType) bool {
+	return parser.peekToken.Type == tokenType
+}
+
+func (parser *Parser) expectPeek(tokenType token.TokenType) bool {
+	if parser.peekTokenIs(tokenType) {
+		parser.nextToken()
+		return true
+	}
+
+	return false
 }
 
 //---[ Parser Helper Methods ]--------------------------------------------------
