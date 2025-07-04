d35322bed1579a8aa14518ccc452252747965153
tontacchi <andy.tonthat14@gmail.com>
2025-07-04 10:45:58 -0700

    added simple let statement test, let statement test helper

---
+++ parser/parser_test.go
@@ -0,0 +1,56 @@
+package parser
+
+import (
+	"testing"
+
+	"monkey/ast"
+	"monkey/lexer"
+)
+
+func TestLetStatements(t *testing.T) {
+	input := `
+let x = 5;
+let y = 10;
+let foobar = 838383;
+`
+	lex := lexer.New(input)
+	parser := New(lex)
+	program := parser.ParseProgram()
+
+	// sanity check: valid program + statement count matches input's
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	} else if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got %d",
+			len(program.Statements),
+		)
+	}
+
+	// check the identifiers was parsed for each let
+	tests := []struct{
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for index, test := range tests {
+		statement := program.Statements[index]
+		if false {
+
+		}
+	}
+}
+
+func testLetStatement(t *testing.T, statement ast.Statement, name string) bool {
+	if statement.TokenLiteral() != "let" {
+		t.Errorf("statement.TokenLiteral not 'let'. got=\"%s\"", statement.TokenLiteral())
+		return false
+	}
+
+	letStatement, ok := statement.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("statement not *ast.LetStatement. got \"%s\"", statement)
+	}
+}
